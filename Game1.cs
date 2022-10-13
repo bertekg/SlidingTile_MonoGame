cfac47376ddf03be0337db0b5393e9916d6cf1ea
bertekg <bartek.grywalski@gmail.com>
2022-10-13 21:53:59 +0200

    Player movement limitation
    Player can move only on floor tiles.
    Add function for getting level initial offset.

--- Game1.cs
+++ Game1.cs
@@ -8,6 +8,7 @@ using System.IO;
 using System.Xml;
 using static SlidingTile_MonoGame.Game1;
 using System.Xml.Serialization;
+using System.Linq;
 
 namespace SlidingTile_MonoGame
 {
@@ -19,22 +20,23 @@ namespace SlidingTile_MonoGame
         private Texture2D _playerTexture2D;
 
         private Vector2 _playerPosition;
+        private Point _playerVirtualPoint;
+        private Point _playerVirtualPointDestination;
 
-        private double _playerSpeed;
         private float _currChangeValue;
 
-        private bool _playerMovePossible;
+        private bool _playerPerformMove;
         private bool _playerMoveInital;
         private float _playerMoveSpeed;
         private double _timeMoveMax, _timeMoveCurrent;
         private float _stepDistans;
-        private Vector2 _moveDirection, _playerPosInit;
+        private Vector2 _moveVerse, _playerPosInit;
 
         private Texture2D _floorTileTexture2D;
 
         private SpriteFont _digitFloor;
 
-        List<Cell> _cells;
+        List<Cell> _floorTiles;
         Vector2 _levelStart;
 
         public Game1()
@@ -46,28 +48,9 @@ namespace SlidingTile_MonoGame
 
         protected override void Initialize()
         {
-            Window.Title = "Sliding Tile - MonoGame (0.1 - 2022.10.12)";
+            Window.Title = "Sliding Tile - MonoGame (0.1.1 - 2022.10.13)";
 
-            _levelStart = new Vector2(5, 6);
-
-            _playerPosition = new Vector2(100 * _levelStart.X, 100 * _levelStart.Y);
-
-            _playerSpeed = 250.0d;
-
-            _playerMovePossible = true;
-            _playerMoveInital = false;
-
-            _timeMoveMax = 0.5d;
-            _timeMoveCurrent = 0.0d;
-            _stepDistans = 100.0f;
-
-            _moveDirection = new Vector2();
-
-            _graphics.PreferredBackBufferWidth = 1280;
-            _graphics.PreferredBackBufferHeight = 720;
-            _graphics.ApplyChanges();
-
-            _cells = new List<Cell>();
+            _floorTiles = new List<Cell>();
             XmlDocument xmlDocument = new XmlDocument();
             xmlDocument.Load("LevelBasic/0_Basic_01.xml");
             string xmlString = xmlDocument.OuterXml;
@@ -78,15 +61,33 @@ namespace SlidingTile_MonoGame
                 XmlSerializer serializer = new XmlSerializer(outType);
                 using (XmlReader reader = new XmlTextReader(read))
                 {
-                    _cells = (List<Cell>)serializer.Deserialize(reader);
+                    _floorTiles = (List<Cell>)serializer.Deserialize(reader);
                     reader.Close();
                 }
                 read.Close();
             }
 
+            _levelStart = GetLevelStart();
+
+            _playerPosition = new Vector2(100 * _levelStart.X, 100 * _levelStart.Y);
+            _playerVirtualPoint = new Point(0, 0);
+            _playerVirtualPointDestination = new Point(0, 0);
+
+            _playerPerformMove = false;
+            _playerMoveInital = false;
+
+            _timeMoveMax = 0.5d;
+            _timeMoveCurrent = 0.0d;
+            _stepDistans = 100.0f;
+
+            _moveVerse = new Vector2();
+
+            _graphics.PreferredBackBufferWidth = 1280;
+            _graphics.PreferredBackBufferHeight = 720;
+            _graphics.ApplyChanges();
+
             base.Initialize();
         }
-
         protected override void LoadContent()
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
@@ -102,30 +103,28 @@ namespace SlidingTile_MonoGame
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
 
-            _currChangeValue = (float)(_playerSpeed * gameTime.ElapsedGameTime.TotalSeconds);
-
-            if (Keyboard.GetState().IsKeyDown(Keys.Up) && _playerMovePossible)
+            if (Keyboard.GetState().IsKeyDown(Keys.Up) && _playerPerformMove == false)
             {
-                _moveDirection = new Vector2(0.0f, -1.0f);
-                _playerMovePossible = false;
+                _moveVerse = new Vector2(0.0f, -1.0f);
+                _playerPerformMove = true;
             }
 
-            if (Keyboard.GetState().IsKeyDown(Keys.Down) && _playerMovePossible)
+            if (Keyboard.GetState().IsKeyDown(Keys.Down) && _playerPerformMove == false)
             {
-                _moveDirection = new Vector2(0.0f, 1.0f);
-                _playerMovePossible = false;
+                _moveVerse = new Vector2(0.0f, 1.0f);
+                _playerPerformMove = true;
             }
 
-            if (Keyboard.GetState().IsKeyDown(Keys.Left) && _playerMovePossible)
+            if (Keyboard.GetState().IsKeyDown(Keys.Left) && _playerPerformMove == false)
             {
-                _moveDirection = new Vector2(-1.0f, 0.0f);
-                _playerMovePossible = false;
+                _moveVerse = new Vector2(-1.0f, 0.0f);
+                _playerPerformMove = true;
             }
 
-            if (Keyboard.GetState().IsKeyDown(Keys.Right) && _playerMovePossible)
+            if (Keyboard.GetState().IsKeyDown(Keys.Right) && _playerPerformMove == false)
             {
-                _moveDirection = new Vector2(1.0f, 0.0f);
-                _playerMovePossible = false;
+                _moveVerse = new Vector2(1.0f, 0.0f);
+                _playerPerformMove = true;
             }
             MovePlayer(gameTime.ElapsedGameTime.TotalSeconds);
 
@@ -138,14 +137,14 @@ namespace SlidingTile_MonoGame
 
             _spriteBatch.Begin();
 
-            for (int i = 0; i < _cells.Count; i++)
+            for (int i = 0; i < _floorTiles.Count; i++)
             {
-                Vector2 finalPosition = new Vector2(100*((int)_levelStart.X + _cells[i].PosX), 100 * ((int)_levelStart.Y - _cells[i].PosY));
+                Vector2 finalPosition = new Vector2(100*((int)_levelStart.X + _floorTiles[i].PosX), 100 * ((int)_levelStart.Y - _floorTiles[i].PosY));
                 _spriteBatch.Draw(_floorTileTexture2D, finalPosition, Color.White);
                 string textInside = string.Empty;
                 Color colorText = new Color();
                 Vector2 vectorTextOffset = new Vector2();
-                switch (_cells[i].Type)
+                switch (_floorTiles[i].Type)
                 {
                     case CellType.None:
                         break;
@@ -155,15 +154,15 @@ namespace SlidingTile_MonoGame
                         vectorTextOffset = new Vector2(35, 15);
                         break;
                     case CellType.Normal:
-                        if (_cells[i].PosX != 0 || _cells[i].PosY != 0)
+                        if (_floorTiles[i].PosX != 0 || _floorTiles[i].PosY != 0)
                         {
-                            textInside = _cells[i].Number.ToString();
+                            textInside = _floorTiles[i].Number.ToString();
                             colorText = Color.Black;
                             vectorTextOffset = new Vector2(35, 15);
                         }
                         else
                         {
-                            textInside = "S" + _cells[i].Number.ToString();
+                            textInside = "S" + _floorTiles[i].Number.ToString();
                             colorText = Color.Green;
                             vectorTextOffset = new Vector2(15, 15);
                         }
@@ -184,26 +183,36 @@ namespace SlidingTile_MonoGame
         }
         private void MovePlayer(double totalSecounds)
         {
-            if(_playerMovePossible == false & _playerMoveInital == false)
+            if(_playerPerformMove == true & _playerMoveInital == false)
             {
-                _playerMoveSpeed = _stepDistans / (float)_timeMoveMax;
-                _timeMoveCurrent = 0.0d;
-                _playerMoveInital = true;
-                _playerPosInit = _playerPosition; 
+                _playerVirtualPointDestination = _playerVirtualPoint + new Point((int)_moveVerse.X, -(int)_moveVerse.Y);
+                bool cellFloorExist = _floorTiles.Any(pos => pos.PosX == _playerVirtualPointDestination.X && pos.PosY == _playerVirtualPointDestination.Y);
+                if (cellFloorExist)
+                {
+                    _playerMoveSpeed = _stepDistans / (float)_timeMoveMax;
+                    _timeMoveCurrent = 0.0d;
+                    _playerMoveInital = true;
+                    _playerPosInit = _playerPosition;
+                }
+                else
+                {
+                    _playerPerformMove = false;
+                }
             }
             _currChangeValue = (float)(_playerMoveSpeed * totalSecounds);
-            if (_playerMovePossible == false & _playerMoveInital == true)
+            if (_playerPerformMove == true & _playerMoveInital == true)
             {
                 if (_timeMoveCurrent <= _timeMoveMax)
                 {
-                    _playerPosition += new Vector2(_currChangeValue * _moveDirection.X, _currChangeValue * _moveDirection.Y);
+                    _playerPosition += new Vector2(_currChangeValue * _moveVerse.X, _currChangeValue * _moveVerse.Y);
                     _timeMoveCurrent += totalSecounds;
                 }
                 else
                 {
-                    _playerPosition = new Vector2(_playerPosInit.X + (_stepDistans * _moveDirection.X), _playerPosInit.Y + (_stepDistans * _moveDirection.Y));
+                    _playerPosition = new Vector2(_playerPosInit.X + (_stepDistans * _moveVerse.X), _playerPosInit.Y + (_stepDistans * _moveVerse.Y));
                     _playerMoveInital = false;
-                    _playerMovePossible = true;
+                    _playerPerformMove = false;
+                    _playerVirtualPoint = _playerVirtualPointDestination;
                 }
             }
         }
@@ -214,6 +223,12 @@ namespace SlidingTile_MonoGame
             public int PosY { get; set; }
             public CellType Type { get; set; }
             public int Number { get; set; }
+        }
+        private Vector2 GetLevelStart()
+        {
+            int offestX = -_floorTiles.Min(posX => posX.PosX);
+            int offsetY = _floorTiles.Max(posY => posY.PosY);
+            return new Vector2(offestX, offsetY);
         }
     }
 }
